7f0bea1fb1c59e43bb1afbc92fdeceadd2030837
Manuel <lestard@esrix.de>
2011-03-14 19:09:39 +0100

    added Overlay on Tab contextmenu

---
+++ createXPI.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+
+set zip=c:\programme\7-zip\7z.exe
+
+set srcdir=%cd%\src\*
+
+set xpi=%cd%\hidetoolbarsforapptabs.xpi
+
+
+
+del %xpi%
+
+
+
+echo source directory: %srcdir%
+echo xpi : %xpi%
+
+echo creating xpi
+%zip% a %xpi% -tzip %srcdir%
