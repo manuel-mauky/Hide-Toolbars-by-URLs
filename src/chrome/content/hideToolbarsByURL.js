bbb8ff359d65cbfff3c36072e9ef0eea745a6e70
Manuel <lestard@esrix.de>
2011-03-31 18:12:45 +0200

    added a reload when you hide a url

--- src/chrome/content/hideToolbarsByURL.js
+++ src/chrome/content/hideToolbarsByURL.js
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is from "Hide Toolbars by URLs" Addon.
+ * The Original Code is from "Hide Toolbars by URL" Addon.
  *
  * The Initial Developer of the Original Code is
  * Manuel Mauky <manuel.mauky@googlemail.com>.
